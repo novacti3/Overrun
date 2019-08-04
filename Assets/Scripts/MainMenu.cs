545a8821c72c3717799b49158bde0916d5a6fdab
novacti3 <honsal3@seznam.cz>
2019-08-04 18:22:27 +0200

    Main Menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+
+public class MainMenu : MonoBehaviour
+{
+    public AudioSource source;
+    public AudioClip musicWithIntro;
+    public AudioClip musicNoIntro;
+    public Animator bgAnim;
+    public float lightningChance = 10f;
+
+    void Start()
+    {
+        source.clip = musicWithIntro;
+        source.Play();
+
+        StartCoroutine(Lightning());
+    }
+
+    void Update()
+    {
+        if (!source.isPlaying)
+        {
+            source.clip = musicNoIntro;
+            source.loop = true;
+            source.Play();
+        }
+    }
+
+    IEnumerator Lightning()
+    {
+        print("Ges");
+        bgAnim.ResetTrigger("lightning");
+        yield return new WaitForSeconds(2f);
+        int num = Random.Range(0, 100);
+        if (num <= lightningChance)
+        {
+            bgAnim.SetTrigger("lightning");
+        }
+        StartCoroutine(Lightning());
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
